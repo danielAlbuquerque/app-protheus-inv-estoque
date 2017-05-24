0e1e6483941db2eb0b7313732aefee2c83d912d9
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2017-05-23 22:40:10 -0300

    Auth

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/timeout';
 
 @Injectable()
 export class AuthService {
   private url_service: string;
 
   constructor(public http: Http) {
-    this.url_service = localStorage.getItem('URL_SERVICE');
+    this.url_service = localStorage.getItem('URL_SERVIDOR');
   }
 
-  login() {
+  login(usr, pass) {
     return new Promise((resolve, reject) => {    
       if(this.url_service != null) {
-        this.http.get(this.url_service + '/users')
+        let token = btoa(usr+':'+pass);
+        let headers = new Headers();
+        headers.append("Authorization", "Basic " + token);
+
+        this.http.get(this.url_service + '/users', {headers: headers})
         .map(res => res.json())
+        .timeout(20000)
         .subscribe(response => {
-          resolve(true);
+          localStorage.setItem('token', token);
+          resolve({success: true, token: token});
         }, err => {
-          reject(err.message);
+          if(err.status == 401) {
+            reject("Acesso negado");
+          } else {
+            reject("Ocorreu um erro ao processar a requisição, tente novamente mais tarde");
+          }
+            
         });
       } else {
         reject("Servidor não configurado");
